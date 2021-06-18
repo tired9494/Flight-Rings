8dc9418a6b5d3f9ced18994561a60d8fb4a18b7a
tired <kubaiolivia@gmail.com>
2021-06-18 11:11:08 +0100

    Add files via upload

---
+++ src/main/java/flight_rings/flight_rings/config/ConfigInstance.java
@@ -0,0 +1,31 @@
+package flight_rings.flight_rings.config;
+
+public class ConfigInstance {
+
+	public float basicExhaustion;
+	public float advancedExhaustion;
+	public boolean basicRecipeUsesEnderEye;
+	public double damageChancePerTick;
+	public String note;
+	public boolean pureRingUsesXP;
+	public float pureRingXPAmount;
+	public String note2;
+	public boolean basicRingHasDurability;
+	public boolean basicRingEnabled;
+	public boolean treasureOnly;
+	
+	public ConfigInstance() {
+		basicExhaustion = 0.15F;
+		advancedExhaustion = 0.01F;
+		basicRingEnabled = true;
+		basicRingHasDurability = true;
+		basicRecipeUsesEnderEye = false;
+		damageChancePerTick = 10.0;
+		note = "^ Percentage chance per tick (20 ticks per second) to lose durability of ring ^";
+		pureRingUsesXP = false;
+		pureRingXPAmount = 0.001F;
+		note2 = "^ With the default of 0.001, each XP level should last 50 seconds. Lower values will make each level last longer ^";
+		treasureOnly = false;
+	}
+	
+}
